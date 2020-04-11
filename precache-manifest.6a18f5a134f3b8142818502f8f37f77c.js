f0d8d8bdf63e8d1109be04effaa8176a8182a8ca
excel61 <ahmadipoor.h@gmail.com>
2020-04-12 03:20:09 +0330

    Updates

---
+++ precache-manifest.6a18f5a134f3b8142818502f8f37f77c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9e5c158aee313f3a258b78eec5a3f4",
+    "url": "/react-robot/index.html"
+  },
+  {
+    "revision": "7c76e55848b444c24d46",
+    "url": "/react-robot/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "f2d331d62f09cac650f6",
+    "url": "/react-robot/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7c76e55848b444c24d46",
+    "url": "/react-robot/static/js/2.7ec24f85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-robot/static/js/2.7ec24f85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d331d62f09cac650f6",
+    "url": "/react-robot/static/js/main.5de812cb.chunk.js"
+  },
+  {
+    "revision": "4e4967496225d53c6217",
+    "url": "/react-robot/static/js/runtime-main.09d819e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-robot/static/media/logo.5d5d9eef.svg"
+  }
+]);
